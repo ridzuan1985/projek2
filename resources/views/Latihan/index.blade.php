b769c7812390f8bfa34e05fb95a805940954a2f6
ridzuan1985 <onemuc@gmail.com>
2024-05-08 10:41:41 +0800

    latihan 2

--- resources/views/Latihan/index.blade.php
+++ resources/views/Latihan/index.blade.php
@@ -1,5 +1,17 @@
 @extends('Layouts.master')
 @section('Content')
+    @php
+    $nama = "Harimau";
+    $bapa = "Malaya";
+@endphp
+
+<div class="text-center">
+    <h1>Nama saya {{$nama}} bin <?php echo $bapa; ?></h1>
+    <p>Selamat Datang</p>
+    <p>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/2e/Tigerramki.jpg" class="img-fluid" alt="Harimau" width="50%" height="50%">
+    </p>
+</div>
     <h3>Hi Ahmad</h3>
     
 @endsection
